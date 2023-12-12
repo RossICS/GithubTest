67ca550766ebbb09ba2d2bb427f41c942f814078
Richard Kaune <32107759+RossICS@users.noreply.github.com>
2023-12-12 11:22:01 -0500

    Documenting GithubTest.java

--- src/main/java/githubtest/GithubTest.java
+++ src/main/java/githubtest/GithubTest.java
@@ -1,12 +1,12 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * December 12, 2023
+ * Testing the github
  */
 package githubtest;
 
 /**
  *
- * @author Administrator
+ * @author RKaune
  */
 public class GithubTest {
 
